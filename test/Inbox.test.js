16cb4ab27f075d4c80156d5acc0afe96abd0ee46
pskanade <k.pranav.apk@gmail.com>
2018-10-14 22:54:19 +0530

    increased the number of event listeners to suppress the warning of data leak.

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,10 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+
+// add more data listeners. Settles the warining regarding the data leak
+require('events').EventEmitter.defaultMaxListeners = 15;
+
 const web3 = new Web3(ganache.provider());
 const {
     interface,
