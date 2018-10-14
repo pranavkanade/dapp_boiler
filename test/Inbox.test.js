a9e1c883532cf0d80af86a1a99a2d6491e65a8fe
pskanade <k.pranav.apk@gmail.com>
2018-10-14 22:12:15 +0530

    get accounts using promise

---
+++ test/Inbox.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+    web3.eth.getAccounts().then(accountslist => {
+        console.log(accountslist);
+    });
+});
+
+describe('Inbox', () => {
+    it('Accounts', () => {
+
+    });
+});
